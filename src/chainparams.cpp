dc18d21a593b77f333a53d76e3bc98bcd09ea154
nlevo <nicolas.levo@gmail.com>
2019-04-04 13:53:15 -0400

    Burn at 46383

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -68,7 +68,7 @@ public:
         consensus.vDeployments[Consensus::DEPLOYMENT_TESTDUMMY].nTimeout = 1230767999; // December 31, 2008
         
         // sprout burn
-        consensus.zResetHeight = 48500;
+        consensus.zResetHeight = 46383;
 
         consensus.nForkStartHeight = 3;
         consensus.nForkHeightRange = 16737;
@@ -184,7 +184,7 @@ public:
         masternodeCollateralNew = 10000;
 
         //Sapling
-        saplingActivationBlock = 48500;
+        saplingActivationBlock = 46383;
 
         // Equihash algo
         eh_epoch_1 = eh200_9;
